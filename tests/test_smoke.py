3aa6d9a075ebb0fe65aa253e0096c5c4bdc2e0a2
Vladyslav Yurchenko <silvia.homam@gmail.com>
2025-06-02 16:31:58 +0300

    Move tests to root and refactor pipeline

---
+++ tests/test_smoke.py
@@ -0,0 +1,74 @@
+import subprocess
+import yaml
+import pytest
+
+NUM_OPS = 100
+THREADS = 2
+CL = "ONE"
+
+def run_command(cmd):
+    result = subprocess.run(cmd, shell=False, text=True, capture_output=True)
+    if result.returncode != 0:
+        print(result.stdout)
+        print(result.stderr)
+        raise subprocess.CalledProcessError(result.returncode, cmd)
+    return result.stdout
+
+def is_snap_installed():
+    try:
+        subprocess.run(["snap", "--version"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return True
+    except Exception:
+        return False
+
+def cassandra_stress_available():
+    try:
+        subprocess.run(["snap", "run", "cassandra.stress", "help"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return True
+    except Exception:
+        return False
+
+def test_install():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+        snap_file = f"./{snapcraft['name']}_{snapcraft['version']}_amd64.snap"
+        subprocess.run(
+            ["sudo", "snap", "install", snap_file, "--devmode"],
+            check=True,
+        )
+
+@pytest.mark.run(after="test_install")
+def test_write_stress():
+    if not is_snap_installed():
+        pytest.fail("[FAILED] snap command not found")
+
+    if not cassandra_stress_available():
+        pytest.fail("[FAILED] cassandra-stress is not installed or not available via snap")
+
+    print("▶ Starting WRITE test...")
+    try:
+        run_command([
+            "snap", "run", "cassandra.stress", "write",
+            f"n={NUM_OPS}",
+            f"cl={CL}",
+            "-rate",
+            f"threads={THREADS}"
+        ])
+    except subprocess.CalledProcessError:
+        pytest.fail("[FAILED] WRITE test failed")
+    print("[SUCCESS] WRITE test completed")
+
+@pytest.mark.run(after="test_install")
+def test_read_stress():
+    print("▶ Starting READ test...")
+    try:
+        run_command([
+            "snap", "run", "cassandra.stress", "read",
+            f"n={NUM_OPS}",
+            f"cl={CL}",
+            "-rate",
+            f"threads={THREADS}"
+        ])
+    except subprocess.CalledProcessError:
+        pytest.fail("[FAILED] READ test failed")
+    print("[SUCCESS] READ test completed")
